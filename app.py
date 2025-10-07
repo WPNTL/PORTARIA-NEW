209373f8b60d9bbfcc096f44d702d10036e0979f
Wesley <wesleypuntel@gmail.com>
2025-10-07 17:42:59 -0300

    Night mode add (parcial) admin still ligth mode (temporarily)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import sqlite3
 import os
 from datetime import datetime
-import bcrypt
+import bcrypt # type: ignore
 from functools import wraps
 
 app = Flask(__name__)
